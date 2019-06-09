45c0b140e76ba9e65f742b6b0bed266963481550
Benedict Mateo <benedictpmateo@gmail.com>
2019-06-09 11:13:49 +0800

    :wrench: added google tag manager

--- common/resources/views/framework.blade.php
+++ common/resources/views/framework.blade.php
@@ -22,6 +22,15 @@
         @if ($settings->has('custom_code.load_css'))
             <link rel="stylesheet" href="{{asset('storage/custom-code/custom-styles.css')}}">
         @endif
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140902921-2"></script>
+        <script>
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+
+             gtag('config', 'UA-140902921-2');
+        </script>
 	</head>
 
     <body id="theme">
